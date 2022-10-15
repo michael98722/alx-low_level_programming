63276f6e2c27f9689b61b646f59fe1fe6577d3b2
michael98722 <michaelagbodza16@gmail.com>
2022-10-14 21:24:30 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,40 +7,26 @@
 
 int main(void)
 {
-	int c, i, k, j;
+	int c, i;
 
-	for (c = 48; c <= 57; c++)
+	for (c = 0; c < 99; c++)
 	{
-		for (i = 48; i <= 57; i++)
+		for (i = 0; i < 99; i++)
 		{
-			for (k = 48; k <= 57; k++)
-			{
-				for (j = 48; j <= 57; j++)
-				{
-					if (((k + j) > (c + i) &&  k >= c) || c < k)
-					{
-						putchar(c);
-						putchar(i);
-						putchar(' ');
-						putchar(k);
-						putchar(j);
+			putchar('0' + c / 10);
+			putchar('0' + c % 10);
 
-						if (c + i + k + j == 227 && c == 57)
-						{
-							break;
-						}
-						else
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-				}
-			}
+			putchar(' ');
+
+			putchar('0' + i / 10);
+			putchar('0' + i % 10);
+
+			if (c == 98 && i == 99)
+				break;
+			putchar(',');
+			putchar(' ');
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
