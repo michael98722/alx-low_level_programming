3855a1722d43dc8876cfd15923dd1ce11e1f9cb3
michael98722 <michaelagbodza16@gmail.com>
2022-10-18 16:47:01 -0700

    I sometimes suffer from insomnia. And..)

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "main.h"
+
 /**
  * print_alphabet - It should be in lowercase, followed by a new line
  */
 
 void print_alphabet(void);
 {
-	char x;
+	char xy;
 
-	for (x = 'a'; x <= 'z'; x++)
-		_putchar(x);
+	for (xy = 'a'; xy <= 'z'; xy++)
+		_putchar(xy);
 
 		_putchar('\n');
 }
