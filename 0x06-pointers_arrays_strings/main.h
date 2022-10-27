c6abaad338ed6bba625a63038619cb5f919074e5
michael98722 <michaelagbodza16@gmail.com>
2022-10-27 16:42:58 +0100

    A dream doesn't become reality

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-
+/**
+ * void prototypes()
+ * int prototypes()
+ * char prototypes()
+ */
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -15,6 +19,6 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-int main(void);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 
-#endif
+#endif /*MAIN_H*/
