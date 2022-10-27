711c2da2b0b6261454665fe0025be275c02f3899
michael98722 <michaelagbodza16@gmail.com>
2022-10-27 14:47:01 +0100

    A dream doesn't become reality

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,7 +9,7 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 	/*
-	 * main - prints a[2] = 98, followed by a new line.
+	 * write your line of code here...
 	 * Remember:
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
