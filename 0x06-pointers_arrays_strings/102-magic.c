2366fe84f14ac4ce23f634cc5d3de2f8c2debfc8
michael98722 <michaelagbodza16@gmail.com>
2022-10-27 18:13:21 +0100

    A dream doesn't become reality

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -3,10 +3,10 @@
 int main(void)
 {
 	int n;
-	int a[5];
+	int i[5];
 	int *p;
 
-	a[2] = 1024;
+	i[2] = 1024;
 	p = &n;
 	/*
 	 * write your line of code here...
@@ -18,6 +18,6 @@ int main(void)
 	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
+	printf("i[2] = %d\n", i[2]);
 	return (0);
 }
