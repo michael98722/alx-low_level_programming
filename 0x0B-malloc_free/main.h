d4d43e9f0e3c27fdc1ef3581dc9a4769f7a13b15
michael98722 <michaelagbodza16@gmail.com>
2022-11-07 02:15:31 -0800

    I will show you how great I am

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -9,6 +9,8 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
+void util(char **, char *);
+void create_word(char **, char *, int, int, int);
 
 
 #endif
